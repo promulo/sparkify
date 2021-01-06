00fab409fafb9a1d52ff16248ba59fc8f570c467
Paulo Barros <promuloo@gmail.com>
2021-01-06 01:18:54 -0300

    Fix DROP statement for PG time table

--- data_modelling_pg/sql_queries.py
+++ data_modelling_pg/sql_queries.py
@@ -4,7 +4,7 @@ songplay_table_drop = "DROP TABLE IF EXISTS songplays"
 user_table_drop = "DROP TABLE IF EXISTS users"
 song_table_drop = "DROP TABLE IF EXISTS songs"
 artist_table_drop = "DROP TABLE IF EXISTS artists"
-time_table_drop = "DROP TABLE IF EXISTS timestamps"
+time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
 
