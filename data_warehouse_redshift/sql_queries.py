899520d8151c3e0c0a649f56b6f4dd128b5be7e8
Paulo Barros <promuloo@gmail.com>
2021-01-14 22:24:24 +0000

    Create staging tables and fix the rest

--- data_warehouse_redshift/sql_queries.py
+++ data_warehouse_redshift/sql_queries.py
@@ -17,75 +17,115 @@ time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
 
-staging_events_table_create = ("""
-""")
+staging_events_table_create = """
+CREATE TABLE IF NOT EXISTS staging_events (
+    id BIGINT IDENTITY(0,1) PRIMARY KEY,
+    artist TEXT,
+    auth TEXT,
+    first_name TEXT,
+    gender VARCHAR(1),
+    item_in_session INT,
+    last_name TEXT,
+    length FLOAT,
+    level TEXT,
+    location TEXT,
+    method TEXT,
+    page TEXT,
+    registration BIGINT,
+    session_id INT,
+    song TEXT,
+    status INT,
+    ts BIGINT,
+    user_agent TEXT,
+    user_id TEXT
+)
+"""
 
-staging_songs_table_create = ("""
-""")
+staging_songs_table_create = """
+CREATE TABLE IF NOT EXISTS staging_songs (
+    id BIGINT IDENTITY(0,1) PRIMARY KEY,
+    num_songs INT,
+    artist_id TEXT,
+    artist_latitude FLOAT,
+    artist_longitude FLOAT,
+    artist_location TEXT,
+    artist_name TEXT,
+    song_id TEXT,
+    title TEXT,
+    duration FLOAT,
+    year INT
+)
+"""
 
 songplay_table_create = """
 CREATE TABLE IF NOT EXISTS songplays (
-    id SERIAL PRIMARY KEY,
-    start_time timestamp NOT NULL,
-    user_id int NOT NULL,
-    level varchar NOT NULL,
-    song_id varchar,
-    artist_id varchar,
-    session_id int NOT NULL,
-    location varchar NOT NULL,
-    user_agent text NOT NULL
+    id BIGINT IDENTITY(0,1) PRIMARY KEY,
+    start_time TIMESTAMP NOT NULL,
+    user_id INT NOT NULL,
+    level VARCHAR NOT NULL,
+    song_id VARCHAR,
+    artist_id VARCHAR,
+    session_id INT NOT NULL,
+    location VARCHAR NOT NULL,
+    user_agent TEXT NOT NULL
 )
 """
 
 user_table_create = """
 CREATE TABLE IF NOT EXISTS users (
-    id int PRIMARY KEY,
-    first_name varchar NOT NULL,
-    last_name varchar NOT NULL,
-    gender varchar(1) NOT NULL,
-    level varchar NOT NULL
+    id INT PRIMARY KEY,
+    first_name VARCHAR NOT NULL,
+    last_name VARCHAR NOT NULL,
+    gender VARCHAR(1) NOT NULL,
+    level VARCHAR NOT NULL
 )
 """
 
 song_table_create = """
 CREATE TABLE IF NOT EXISTS songs (
-    id varchar PRIMARY KEY,
-    title varchar NOT NULL,
-    artist_id varchar NOT NULL,
-    year int NOT NULL,
-    duration float NOT NULL
+    id VARCHAR PRIMARY KEY,
+    title VARCHAR NOT NULL,
+    artist_id VARCHAR NOT NULL,
+    year INT NOT NULL,
+    duration FLOAT NOT NULL
 )
 """
 
 artist_table_create = """
 CREATE TABLE IF NOT EXISTS artists (
-    id varchar PRIMARY KEY,
-    name varchar NOT NULL,
-    location varchar NOT NULL,
-    latitude float,
-    longitude float
+    id VARCHAR PRIMARY KEY,
+    name VARCHAR NOT NULL,
+    location VARCHAR NOT NULL,
+    latitude FLOAT,
+    longitude FLOAT
 )
 """
 
 time_table_create = """
 CREATE TABLE IF NOT EXISTS time (
-    timestamp bigint PRIMARY KEY,
-    hour int NOT NULL,
-    day int NOT NULL,
-    week int NOT NULL,
-    month int NOT NULL,
-    year int NOT NULL,
-    weekday int NOT NULL
+    timestamp BIGINT PRIMARY KEY,
+    hour INT NOT NULL,
+    day INT NOT NULL,
+    week INT NOT NULL,
+    month INT NOT NULL,
+    year INT NOT NULL,
+    weekday INT NOT NULL
 )
 """
 
 # STAGING TABLES
 
-staging_events_copy = ("""
-""").format()
+staging_events_copy = f"""
+COPY staging_events FROM '{config.get("S3", "LOG_DATA")}'
+CREDENTIALS 'aws_iam_role={config.get("IAM_ROLE", "ARN")}'
+COMPUPDATE OFF REGION 'eu-central-1';
+"""
 
-staging_songs_copy = ("""
-""").format()
+staging_songs_copy = f"""
+COPY staging_songs FROM '{config.get("S3", "SONG_DATA")}'
+CREDENTIALS 'aws_iam_role={config.get("IAM_ROLE", "ARN")}'
+COMPUPDATE OFF REGION 'eu-central-1';
+"""
 
 # FINAL TABLES
 
