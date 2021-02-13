9836ba997cbfdd97b85d8084346f66fcf12594d1
Paulo Barros <promuloo@gmail.com>
2021-02-13 13:25:23 +0000

    Create users and time tables

--- data_lake_spark/etl.py
+++ data_lake_spark/etl.py
@@ -3,7 +3,7 @@ import os
 from datetime import datetime
 
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, date_format, dayofmonth, hour, month, udf, weekofyear, year
+from pyspark.sql.functions import IntegerType, TimestampType, lit, udf
 
 config = configparser.ConfigParser()
 config.read('dl.cfg')
@@ -20,13 +20,31 @@ def create_spark_session():
     return spark
 
 
+@udf(TimestampType())
+def get_timestamp(epoch_ms):
+    return datetime.fromtimestamp(epoch_ms / 1000)
+
+
+@udf(IntegerType())
+def get_date_part(date, key):
+    parts = {
+        "hour": date.hour,
+        "day": date.day,
+        "week": date.isocalendar()[1],
+        "month": date.month,
+        "year": date.year,
+        "weekday": date.isoweekday()
+    }
+    return parts[key]
+
+
 def process_song_data(spark, input_data, output_data):
     df = spark.read.json(f"{input_data}/song-data/A/A/A/")
 
     songs_table = df.select("song_id", "title", "artist_id", "year", "duration")
-    songs_table.write\
-        .partitionBy("year", "artist_id")\
-        .mode("overwrite")\
+    songs_table.write \
+        .partitionBy("year", "artist_id") \
+        .mode("overwrite") \
         .parquet(f"{output_data}/songs.parquet")
 
     artists_table = df.select(
@@ -39,44 +57,50 @@ def process_song_data(spark, input_data, output_data):
     artists_table.write.mode("overwrite").parquet(f"{output_data}/artists.parquet")
 
 
-# def process_log_data(spark, input_data, output_data):
-#     # get filepath to log data file
-#     log_data =
+def process_log_data(spark, input_data, output_data):
+    # get filepath to log data file
+    log_data = f"{input_data}/log-data/"
 
-#     # read log data file
-#     df =
+    # read log data file
+    df = spark.read.json(log_data)
 
-#     # filter by actions for song plays
-#     df =
+    # filter by actions for song plays
+    df = df.filter(df.page == "NextSong")
+    df.createOrReplaceTempView("logs_nextsong")
 
-#     # extract columns for users table
-#     artists_table =
+    # extract columns for users table
+    users_table = spark.sql("""
+        SELECT DISTINCT userId, firstName, lastName, gender, level
+        FROM next_songs
+    """)
 
-#     # write users table to parquet files
-#     artists_table
+    # write users table to parquet files
+    users_table.write.mode("overwrite").parquet(f"{output_data}/users.parquet")
 
-#     # create timestamp column from original timestamp column
-#     get_timestamp = udf()
-#     df =
+    # create timestamp column from original timestamp column
+    df = df.withColumn("timestamp", get_timestamp("ts"))
 
-#     # create datetime column from original timestamp column
-#     get_datetime = udf()
-#     df =
+    date_parts = ["hour", "day", "week", "month", "year", "weekday"]
+    for part in date_parts:
+        df = df.withColumn(part, get_date_part("timestamp", lit(part)))
 
-#     # extract columns to create time table
-#     time_table =
+    # extract columns to create time table
+    time_table = df.select("ts", *date_parts)
 
-#     # write time table to parquet files partitioned by year and month
-#     time_table
+    # # write time table to parquet files partitioned by year and month
+    time_table.write \
+        .partitionBy("year", "month") \
+        .mode("overwrite") \
+        .parquet(f"{output_data}/time.parquet")
 
-#     # read in song data to use for songplays table
-#     song_df =
+    # # read in song data to use for songplays table
+    # song_df =
 
-#     # extract columns from joined song and log datasets to create songplays table
-#     songplays_table =
+    # # extract columns from joined song and log datasets to create songplays table
+    # songplays_table =
 
-#     # write songplays table to parquet files partitioned by year and month
-#     songplays_table
+    # # write songplays table to parquet files partitioned by year and month
+    # songplays_table
 
 
 def main():
