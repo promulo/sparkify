69a5a63e1cdff4ff6beecf7ad67cd86bc61251a5
Paulo Barros <promuloo@gmail.com>
2021-02-14 01:13:36 +0000

    Update functions

--- data_lake_spark/etl.py
+++ data_lake_spark/etl.py
@@ -39,80 +39,59 @@ def get_date_part(date, key):
 
 
 def process_song_data(spark, input_data, output_data):
-    df = spark.read.json(f"{input_data}/song-data/A/A/A/")
+    df = spark.read.json(f"{input_data}/song_data/*/*/*")
     df.createOrReplaceTempView("songs_data")
 
     songs_table = spark.sql("""
         SELECT DISTINCT song_id AS id, title, artist_id, year, duration
         FROM songs_data
     """)
-    songs_table.printSchema()
-#     df.select("song_id", "title", "artist_id", "year", "duration")
     songs_table.write.partitionBy("year", "artist_id").mode("overwrite").parquet(f"{output_data}/songs.parquet")
 
     artists_table = spark.sql("""
         SELECT DISTINCT artist_id AS id, artist_name AS name, artist_location AS location, artist_latitude AS latitude, artist_longitude AS longitude
         FROM songs_data
     """)
-    artists_table.printSchema()
-#     df.select("artist_id", "artist_name", "artist_location", "artist_latitude", "artist_longitude")
     artists_table.write.mode("overwrite").parquet(f"{output_data}/artists.parquet")
 
 
 def process_log_data(spark, input_data, output_data):
-    # get filepath to log data file
-    log_data = f"{input_data}/log-data/"
+    log_data = f"{input_data}/log_data/*/*"
 
-    # read log data file
     df = spark.read.json(log_data)
 
-    # filter by actions for song plays
     df = df.filter(df.page == "NextSong")
     df.createOrReplaceTempView("logs_nextsong")
 
-    # extract columns for users table
     users_table = spark.sql("""
         SELECT DISTINCT userId AS id, firstName AS first_name, lastName AS last_name, gender, level
         FROM logs_nextsong
     """)
-    users_table.printSchema()
-
-    # write users table to parquet files
     users_table.write.mode("overwrite").parquet(f"{output_data}/users.parquet")
 
-    # create timestamp column from original timestamp column
     df = df.withColumn("timestamp", get_timestamp("ts"))
 
     date_parts = ["hour", "day", "week", "month", "year", "weekday"]
     for part in date_parts:
         df = df.withColumn(part, get_date_part("timestamp", lit(part)))
 
-    # extract columns to create time table
     time_table = df.select("ts", *date_parts)
-    time_table.printSchema()
-
-    # write time table to parquet files partitioned by year and month
     time_table.write.partitionBy("year", "month").mode("overwrite").parquet(f"{output_data}/time.parquet")
 
     df.createOrReplaceTempView("logs_nextsong")
 
-    # read in song data to use for songplays table
     songs_df = spark.read.parquet(f"{output_data}/songs.parquet")
     songs_df.createOrReplaceTempView("songs")
 
     artists_df = spark.read.parquet(f"{output_data}/artists.parquet")
     artists_df.createOrReplaceTempView("artists")
 
-    # extract columns from joined song and log datasets to create songplays table
     songplays_table = spark.sql("""
         SELECT timestamp AS start_time, userId AS user_id, level, s.id AS song_id, a.id AS artist_id, sessionId AS session_id, l.location, userAgent AS user_agent
         FROM logs_nextsong l
         JOIN songs s ON s.title = l.song
         JOIN artists a ON a.name = l.artist
     """)
-
-    songplays_table.printSchema()
-    # write songplays table to parquet files partitioned by year and month
     songplays_table \
         .withColumn("year", year(col("start_time"))) \
         .withColumn("month", month(col("start_time"))) \
@@ -124,8 +103,9 @@ def process_log_data(spark, input_data, output_data):
 
 def main():
     spark = create_spark_session()
-    input_data = "s3a://udacity-dend/"
-    output_data = "out"
+
+    input_data = "s3a://udacity-dend"
+    output_data = "s3a://udacity-paulo/sparkify/tables"
 
     process_song_data(spark, input_data, output_data)
     process_log_data(spark, input_data, output_data)
